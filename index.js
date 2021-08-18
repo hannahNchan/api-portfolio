23883605ee683ddb6e4b5e9d083f48605e6e5b9e
Hannah <hannah.n.kahnwald@gmail.com>
2021-08-18 00:05:19 -0500

    Add headers on response

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ const requestListener = function (req, res) {
   res.setHeader("Content-Type", "application/json");
   switch (req.url) {
     case "/books":
-      res.writeHead(200);
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'bacon'
+      });
       res.end(books);
       break
     case "/authors":
-      res.writeHead(200);
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'bacon'
+      });
       res.end(authors);
       break
   }
